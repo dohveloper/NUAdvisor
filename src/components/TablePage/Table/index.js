690123ede830527c0c08fca9bfa5cc0e61db95fa
dohveloper <dohveloper@gmail.com>
2020-11-06 16:40:17 -0500

    Table

--- src/components/TablePage/Table/index.js
+++ src/components/TablePage/Table/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useTable } from 'react-table';
+import { COLUMNS } from '../columns';
 
 function Table() {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => STUDENTS, []);
+
+  const tableInstance = useTable({
+    columns: COLUMNS,
+    data: STUDENTS
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
   return (
-    <div>
-      <div>Table</div>
-    </div>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
---
+++ src/components/TablePage/columns.js
@@ -0,0 +1,42 @@
+export const COLUMNS = [
+  {
+    Header: 'NUID',
+    accessor: 'id'
+  },
+  {
+    Header: 'Name',
+    accessor: 'name'
+  },
+  {
+    Header: 'Email',
+    accessor: 'email'
+  },
+  {
+    Header: 'CRN1',
+    accessor: 'crn1'
+  },
+  {
+    Header: 'CRN2',
+    accessor: 'crn2'
+  },
+  {
+    Header: 'CRN3',
+    accessor: 'crn3'
+  },
+  {
+    Header: 'CRN4',
+    accessor: 'crn4'
+  },
+  {
+    Header: 'CRN5',
+    accessor: 'crn5'
+  },
+  {
+    Header: 'CRN6',
+    accessor: 'crn6'
+  },
+  {
+    Header: 'Status',
+    accessor: 'status'
+  }
+];
