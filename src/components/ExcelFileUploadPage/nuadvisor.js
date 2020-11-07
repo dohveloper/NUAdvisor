101f1a4d93831997debc2f3d70f46e17b1448e12
dohveloper <dohveloper@gmail.com>
2020-11-07 17:22:33 -0500

    Logic setAvailableCourses with refactored code

--- src/components/ExcelFileUploadPage/nuadvisor.js
+++ src/components/ExcelFileUploadPage/nuadvisor.js
@@ -1,6 +1,6 @@
 export let setAvailableCourses = (courses, curriculums) => {
-  let isCourseExist = false;
   let subjectCode;
+  let courseNumbers;
 
   /*
   [purpose]: Get available modules & courses for curriculums for this term (is class of Module courseOption exist)
@@ -13,41 +13,17 @@ export let setAvailableCourses = (courses, curriculums) => {
       let selectedModule = curriculum.modules[i];
       //Check whether if there are courses for any of courses Options
       selectedModule.courseOptions.forEach(courseOption => {
-        /*
-        //Case 1) if courseOption is like ITC*,PHL*, then include all of that courses.
         if (courseOption.includes('*')) {
-          //Extract subjectCode ex)ITC* -> ITC
+          //Case 1. CourseOption with *
+          //1.1 Extract subjectCode ex)ITC* -> ITC
           subjectCode = courseOption.substr(0, courseOption.indexOf('*'));
-          //Get list of course's combinedCourseNumber that include subjectCode(ex. ITC)
-          let courseNumbers = Object.keys(courses).filter(key => key.includes(subjectCode));
-          courseNumbers.forEach(courseNumber => {
-            let course = courses[courseNumber];
-            let availableCourse = { combinedCourseNumber: courseNumber, classes: course.classes };
-            selectedModule.availableCourses.push(availableCourse);
-            selectedModule.isAvailableCourseExist = true;
-          });
-        } else {
-          //Case 2)if courseOption is like ITC2300
-          //Check if classes of that course exist in this term
-          let isCourseExist = courses.hasOwnProperty(courseOption);
-          if (isCourseExist) {
-            //1) Add data to available Courses
-            let course = courses[courseOption];
-            let availableCourse = { combinedCourseNumber: courseOption, classes: course.classes };
-            selectedModule.availableCourses.push(availableCourse);
-            //2) Change isAvailableCourseExist to true
-            selectedModule.isAvailableCourseExist = true;
-          }
-        }*/
-        let courseNumbers;
-        if (courseOption.includes('*')) {
-          //Extract subjectCode ex)ITC* -> ITC
-          subjectCode = courseOption.substr(0, courseOption.indexOf('*'));
-          //Get list of course's combinedCourseNumber that include subjectCode(ex. ITC)
+          //1.2 Get list of course's combinedCourseNumber that include subjectCode(ex. ITC)
           courseNumbers = Object.keys(courses).filter(key => key.includes(subjectCode));
         } else {
+          //Case 2. CourseOption with *
           courseNumbers = Object.keys(courses).filter(key => key === courseOption);
         }
+        //Add availableCourses & Change isAvailableCourseExist to true
         courseNumbers.forEach(courseNumber => {
           let course = courses[courseNumber];
           let availableCourse = { combinedCourseNumber: courseNumber, classes: course.classes };
